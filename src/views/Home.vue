7e0b2f2356a859cb9529ef3451f7c2fa0a354530
sam921611 <sam921611@kimo.com>
2020-02-15 18:48:53 +0800

    修正Home頁news區bug
    修正Home頁news區bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,9 +16,7 @@
 					</v-carousel>
 					<v-list two-line>
 						<v-list-item>
-							<v-list-item-avatar>
-								<v-img src="https://cdn.unwire.hk/wp-content/uploads/2019/10/Best-Free-VPN-3-920x613.jpg"></v-img>
-							</v-list-item-avatar>
+							<v-list-item-avatar><v-img src="https://cdn.unwire.hk/wp-content/uploads/2019/10/Best-Free-VPN-3-920x613.jpg"></v-img></v-list-item-avatar>
 							<v-list-item-content>
 								<v-list-item-title>LinkToWorld.tech</v-list-item-title>
 								<v-list-item-subtitle>Creator</v-list-item-subtitle>
@@ -33,42 +31,21 @@
 			<v-flex xs12 sm6>
 				<v-card class="mt-6" rounded outlined>
 					<v-card-title class="mb-2">
-						<div class="questrial body-2 font-weight-bold">Announcements 公告區 </div>
+						<div class="questrial body-2 font-weight-bold">Announcements 公告區</div>
 						<v-spacer></v-spacer>
-						<v-btn to="/product" small>
-							<v-icon small>open_in_new</v-icon>
-						</v-btn>
+						<v-btn to="/product" small><v-icon small>open_in_new</v-icon></v-btn>
 					</v-card-title>
 					<v-divider></v-divider>
 					<!--循環新聞-->
-					<div class="circle" v-for="item in news" :key="item.title">
-						<v-list-item dense>
-							<v-list-item-icon class="mt-4">
-								<v-icon large>supervised_user_circle</v-icon>
-							</v-list-item-icon>
-							<v-list-item-content>
-								<div class="questrial caption text--grey">{{item.title}}
-									<routerlink to="">
-										<div class="indigo--text font-weight-bold"> See Details</div>
-									</routerlink>
-								</div>
-							</v-list-item-content>
-							<v-list-item-action>
-								<v-btn icon @click="show = !show">
-									<v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-								</v-btn>
-							</v-list-item-action>
-						</v-list-item>
 
-						<v-expand-transition>
-							<div v-show="show">
-								<v-divider></v-divider>
-								<v-card-text>
-									{{item.content}}
-								</v-card-text>
-							</div>
-						</v-expand-transition>
-					</div>
+					<v-expansion-panels popout>
+						<v-expansion-panel v-for="(item, i) in news" :key="i">
+							<v-expansion-panel-header>{{item.title}}</v-expansion-panel-header>
+							<v-expansion-panel-content>
+								{{item.content}}
+							</v-expansion-panel-content>
+						</v-expansion-panel>
+					</v-expansion-panels>
 				</v-card>
 			</v-flex>
 			<!---------------->
@@ -93,7 +70,6 @@
 					</v-list>
 				</v-card>
 			</v-flex>-->
-			
 		</v-layout>
 	</v-container>
 	<!--------------------->
@@ -109,36 +85,38 @@ components: {
 </script>-->
 
 <script>
-	export default {
-		data() {
-			return {
-				//				colors: [
-				//					'green',
-				//					'secondary',
-				//					'yellow darken-4',
-				//					'red lighten-2',
-				//					'orange darken-1',
-				//				],
-				show: false,
-				news: [{
-						title: 'We are now testing features and will release soon!',
-						content: 'vpn news test'
-					},
-					{
-						title: 'news2',
-						content: 'vpn news test2'
-					}
-				],
-				//				------
-				cycle: true,
-				slides: [
-					'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
-					'https://affnotes.com/wp-content/uploads/2018/05/vpn-guide.jpg',
-					'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
-					'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
-					'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
-				],
-			}
-		},
-	}
+export default {
+	data() {
+		return {
+			//				colors: [
+			//					'green',
+			//					'secondary',
+			//					'yellow darken-4',
+			//					'red lighten-2',
+			//					'orange darken-1',
+			//				],
+			show: false,
+			news: [
+				{
+					title: 'We are now testing features and will release soon!',
+					content: 'vpn news test'
+				},
+				{
+					title: 'news2',
+					content: 'vpn news test2'
+				}
+			],
+			//				------
+			cycle: true,
+			slides: [
+				'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
+				'https://affnotes.com/wp-content/uploads/2018/05/vpn-guide.jpg',
+				'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
+				'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg',
+				'https://i.epochtimes.com/assets/uploads/2018/01/7f680532e307b97a065e73919e4454c6-600x400.jpg'
+			]
+		};
+	},
+	created() {}
+};
 </script>
